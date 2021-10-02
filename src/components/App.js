ca6ff63ff8bd906868905624cef176a989848fef
roshin <imroshin@gmail.com>
2021-10-02 21:11:50 +0530

    barebone structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
