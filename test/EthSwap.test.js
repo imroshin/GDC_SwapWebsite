99c06c297a7ed3723a772bfbbc5b7e10fd9b1e91
roshin <imroshin@gmail.com>
2021-10-03 21:26:34 +0530

    added before hook, web3 lib

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,4 +1,5 @@
 const { assert } = require('chai')
+const { default: Web3 } = require('web3')
 
 const Token = artifacts.require('Token')
 const EthSwap = artifacts.require('EthSwap')
@@ -7,11 +8,22 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
+function tokens(n){
+    return web3.utils.toWei(n,'ether')
+}
+
     contract('EthSwap',(accounts)=>{
+        let token,ethSwap
+
+        before(async () => {
+            token = await Token.new()
+            ethSwap = await EthSwap.new(token.address)
+            //transfer all tokens to EthSwap (1 million)
+            await token.transfer(ethSwap.address, tokens('1000000'))
+        })
 
         describe('Token deployment',async () => {
             it('contract has a name', async () => {
-                let token = await Token.new()
                 const name = await token.name()
                 assert.equal(name,'GDC Token')
             })
@@ -20,16 +32,12 @@ require('chai')
         describe('EthSwap deployment',async () => {
 
             it('contract has a name', async () => {
-                let ethSwap = await EthSwap.new()
                 const name = await ethSwap.name()
                 assert.equal(name,'EthSwap Instant Exchange')
             })
             it('contract has tokens',async () => {
-                let token = await Token.new()
-                let ethSwap = await EthSwap.new()
-                await token.transfer(ethSwap.address,'1000000000000000000000000')
                 let balance = await token.balanceOf(ethSwap.address)
-                assert.equal(balance.toString(),'1000000000000000000000000')
+                assert.equal(balance.toString(),tokens('1000000'))
             })
 
         })
