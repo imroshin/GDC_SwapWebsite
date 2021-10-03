5ef15f1030f9c48f9bde228f3474ab29c42b8732
roshin <imroshin@gmail.com>
2021-10-03 21:43:23 +0530

    added buytokens()

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -5,8 +5,17 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint public rate = 100;     // Redemption rate = number of tokens user receive for 1 ether
 
     constructor(Token _token) public{
         token = _token;
+    }
+
+    function buyTokens() public payable{
+        //calculate the number of tokens to buy
+
+        // tokenAmount = amount of ethereum * redemption rate
+        uint tokenAmount = msg.value * rate; // msg.value feeds the amount of ether sent when this ()n was called
+        token.transfer(msg.sender, tokenAmount);
     }
 }
