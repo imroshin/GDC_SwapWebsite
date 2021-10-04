d343c56c5da4324dd571ceacab8c96b65b7f5bb2
roshin <imroshin@gmail.com>
2021-10-04 12:19:11 +0530

    added test cases for sellTokens()

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -69,5 +69,39 @@ function tokens(n){
                 assert.equal(event.rate.toString(),'100')
             })
         })
+
+        describe('sellTokens()', async () => {
+            let result
+
+            before(async ()=> {
+                //investor must approve tokens before the purchase
+                await token.approve(ethSwap.address, tokens('100'),{from: investor})
+                //investore sells the token
+                result = await ethSwap.sellTokens(tokens('100'), {from: investor})
+            })
+            
+            it('Allows user to instantly sell tokens to ethSwap for a fixed price' , async () => {
+                //check investor token balance after purchase
+                let investorBalance = await token.balanceOf(investor)
+                assert.equal(investorBalance.toString(), tokens('0'))
+                
+                //Check ethSwap balance after selling
+                let ethSwapBalance
+                ethSwapBalance = await token.balanceOf(ethSwap.address)
+                assert.equal(ethSwapBalance.toString(), tokens('1000000'))
+                ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+                assert.equal(ethSwapBalance.toString(), web3.utils.toWei('0', 'Ether'))
+
+                // check logs to ensure events was emitted with correct data 
+                const event = result.logs[0].args
+                assert.equal(event.account, investor)
+                assert.equal(event.token,token.address)
+                assert.equal(event.amount.toString(),tokens('100').toString())
+                assert.equal(event.rate.toString(),'100')
+
+                //FAILURE: investor can't sell more tokens than they have
+                await ethSwap.sellTokens(tokens('500'),{from : investor}).should.be.rejected;
+            })
+        })
         
     })
