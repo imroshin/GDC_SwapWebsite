f2182933189a22b5e10a255972ede04172af28ee
roshin <imroshin@gmail.com>
2021-10-04 12:16:28 +0530

    function to sell tokens added

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -7,7 +7,14 @@ contract EthSwap {
     Token public token;
     uint public rate = 100;     // Redemption rate = number of tokens user receive for 1 ether
 
-    event TokenPurchased(
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    event TokensSold(
         address account,
         address token,
         uint amount,
@@ -30,6 +37,25 @@ contract EthSwap {
         token.transfer(msg.sender, tokenAmount);
 
         //Emit an event
-        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _amount) public{
+        //user can't sell more tokens than they have
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        //calculate the amount of ether to redeem
+        uint etherAmount = _amount /rate ;
+
+        // Require that ethSwap has enough ether
+        require(address(this).balance >= etherAmount);
+
+        //perform sale
+        token.transferFrom(msg.sender,address(this), _amount); // transferFrom(from,to,value); address(this) is the address of the SC
+        msg.sender.transfer(etherAmount);  // this transfer function is ether, not related to erc20 like the one above
+
+        //Emit an event
+        emit TokensSold(msg.sender, address(token), _amount, rate);
+
     }
 }
